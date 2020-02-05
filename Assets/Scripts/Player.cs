24e56f06646980a885f04dff158b9911a761da66
Vamshi Krishna <vamshi.krishna@live.in>
2020-02-05 11:52:42 +0530

    Code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Direction
-{
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
-    NONE
-}
-
 public class Player : MonoBehaviour
 {
     private GameObject gridGO = null;
@@ -18,8 +9,6 @@ public class Player : MonoBehaviour
     private int rows = 0;
     private int columns = 0;
     private int destinationIndex = 0;
-    private Direction direction = Direction.NONE;
-    private Direction lastDirection = Direction.NONE;
 
     public void StartGame(GameObject gridGO, int startIndex, int destinationIndex, int rows, int columns)
     {
@@ -28,73 +17,6 @@ public class Player : MonoBehaviour
         this.destinationIndex = destinationIndex;
         this.rows = rows;
         this.columns = columns;
-
-        //gridGO.transform.GetChild(destinationIndex).GetComponent<Tile>().Weight = 0;
-
-        //SetWeightAndDirection(destinationIndex);
-       // FillColumnsToRight(1, destinationIndex);
-       // FillColumnsToLeft(1, destinationIndex);
-       // FillColumnsToUp(1, destinationIndex);
-       // FillColumnsToDown(1, destinationIndex);
-
-        /*var gridT = gridGO.transform;
-        for(int k=0;k<rows*columns;++k)
-        {
-            for(int i=0;i<gridT.childCount;++i)
-            {
-                if (i == startIndex || i == destinationIndex)
-                    continue;
-                
-
-                if(gridT.GetChild(i).GetComponent<Tile>().Weight == 1)
-                {
-                    int minWeight = rows * columns; // max number
-                    if (IsValidRight(i) && IsTileWalkable(i+1))
-                    {
-                        if(gridT.GetChild(i + 1).GetComponent<Tile>().Weight != 1)
-                        minWeight = Mathf.Min(minWeight, gridT.GetChild(i + 1).GetComponent<Tile>().Weight);
-                    }
-                    if (IsValidLeft(i) && IsTileWalkable(i-1))
-                    {
-                        if(gridT.GetChild(i - 1).GetComponent<Tile>().Weight != 1)
-                        minWeight = Mathf.Min(minWeight, gridT.GetChild(i - 1).GetComponent<Tile>().Weight);
-                    }
-                    if (IsValidUp(i) && IsTileWalkable(i+columns))
-                    {
-                        if(gridT.GetChild(i + columns).GetComponent<Tile>().Weight != 1)
-                        minWeight = Mathf.Min(minWeight, gridT.GetChild(i + columns).GetComponent<Tile>().Weight);
-                    }
-                    if (IsValidDown(i) && IsTileWalkable(i-columns))
-                    {
-                        if(gridT.GetChild(i - columns).GetComponent<Tile>().Weight != 1)
-                        minWeight = Mathf.Min(minWeight, gridT.GetChild(i - columns).GetComponent<Tile>().Weight);
-                    }
-
-                    if (minWeight != rows * columns)
-                    {
-                        gridT.GetChild(i).GetComponent<Tile>().SetWeight(minWeight);
-                    }
-
-                }
-            }
-        }*/
-
-
-        /*var currentRow = currentIndex / columns;
-        var destinationRow = destinationIndex / columns;
-        if (currentRow < destinationRow)
-            direction = Direction.UP;
-        else if (currentRow > destinationRow)
-            direction = Direction.DOWN;
-        else
-        {
-            if (currentIndex < destinationIndex)
-                direction = Direction.RIGHT;
-            else
-                direction = Direction.LEFT;
-        }*/
-
-        //InvokeRepeating("Move", 0.0f, 1.0f);
     }
 
     private void Update()
@@ -104,70 +26,52 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
             FillColumnsToRight(1, destinationIndex);
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
             FillColumnsToLeft(1, destinationIndex);
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
             FillColumnsToUp(1, destinationIndex);
+
         if (Input.GetKeyDown(KeyCode.DownArrow))
             FillColumnsToDown(1, destinationIndex);
 
         if (Input.GetKeyDown(KeyCode.O))
         {
-            Debug.Log("Called");
             var gridT = gridGO.transform;
             for (int i = 0; i < gridT.childCount; ++i)
             {
                 if (i == currentIndex || i == destinationIndex)
                     continue;
 
-
                 if (gridT.GetChild(i).GetComponent<Tile>().Weight == 1)
                 {
                     int minWeight = rows * columns; // max number
-                    Debug.Log("index start");
-                    Debug.Log("index" + i + "weight" + minWeight);
                     if (IsValidRight(i) && IsTileWalkable(i + 1))
                     {
-                        Debug.Log(gridT.GetChild(i + 1).GetComponent<Tile>().Weight);
                         if (gridT.GetChild(i + 1).GetComponent<Tile>().Weight != 1)
                             minWeight = Mathf.Min(minWeight, gridT.GetChild(i + 1).GetComponent<Tile>().Weight);
-                        Debug.Log("right index" + i + "weight" + minWeight);
                     }
                     if (IsValidLeft(i) && IsTileWalkable(i - 1))
                     {
                         if (gridT.GetChild(i - 1).GetComponent<Tile>().Weight != 1)
                             minWeight = Mathf.Min(minWeight, gridT.GetChild(i - 1).GetComponent<Tile>().Weight);
-                        Debug.Log("left index" + i + "weight" + minWeight);
                     }
                     if (IsValidUp(i) && IsTileWalkable(i + columns))
                     {
                         if (gridT.GetChild(i + columns).GetComponent<Tile>().Weight != 1)
                             minWeight = Mathf.Min(minWeight, gridT.GetChild(i + columns).GetComponent<Tile>().Weight);
-                        Debug.Log("up index" + i + "weight" + minWeight);
                     }
                     if (IsValidDown(i) && IsTileWalkable(i - columns))
                     {
                         if (gridT.GetChild(i - columns).GetComponent<Tile>().Weight != 1)
                             minWeight = Mathf.Min(minWeight, gridT.GetChild(i - columns).GetComponent<Tile>().Weight);
-                        Debug.Log("down index" + i + "weight" + minWeight);
                     }
-
-                    Debug.Log("index" + i+ "weight" + minWeight);
 
                     if (minWeight != rows * columns)
                     {
                         gridT.GetChild(i).GetComponent<Tile>().SetWeight(minWeight);
-                        //bAllFilled = bAllFilled && true;
                     }
-                    else
-                    {
-                        //bAllFilled = bAllFilled && false;
-                    }
-
-                }
-                else
-                {
-                    //bAllFilled = bAllFilled && true;
                 }
             }
         }
@@ -230,299 +134,17 @@ public class Player : MonoBehaviour
         transform.position = gridGO.transform.GetChild(currentIndex).position;
     }
 
-    private int GetNextIndex(List<Direction> attempted)
-    {
-        var currentRow = currentIndex / columns;
-        var destinationRow = destinationIndex / columns;
-        var nextIndex = -1;
-
-        if(attempted.Count == 0)
-        {
-            lastDirection = direction;
-            if (currentRow < destinationRow && direction != Direction.DOWN)
-                direction = Direction.UP;
-            else if (currentRow > destinationRow && direction != Direction.UP)
-                direction = Direction.DOWN;
-            else
-            {
-                var equivalentIndex = currentIndex + (columns * (destinationRow - currentRow));
-                if (equivalentIndex < destinationIndex)
-                    direction = Direction.RIGHT;
-                else
-                    direction = Direction.LEFT;
-            }
-        }
-
-        switch (direction)
-        {
-            case Direction.UP:
-                nextIndex = currentIndex + columns;
-                break;
-            case Direction.DOWN:
-                nextIndex = currentIndex - columns;
-                break;
-            case Direction.LEFT:
-                nextIndex = currentIndex - 1;
-                break;
-            case Direction.RIGHT:
-                nextIndex = currentIndex + 1;
-                break;
-        }
-
-
-        if (IsValidNextIndex(nextIndex))
-        {
-            return nextIndex;
-        }
-        else
-        {
-            attempted.Add(direction);
-            var equivalentIndex = currentIndex + (columns * (destinationRow - currentRow));
-
-            if (currentRow > destinationRow)
-            {
-                if (!attempted.Contains(Direction.DOWN) && lastDirection != Direction.DOWN)
-                {
-                    direction = Direction.DOWN;
-                    nextIndex = GetNextIndex(attempted);
-                    return nextIndex;
-                }
-                else
-                {
-                    if (equivalentIndex <= destinationIndex)
-                    {
-                        if (!attempted.Contains(Direction.RIGHT))
-                        {
-                            direction = Direction.RIGHT;
-                            nextIndex = GetNextIndex(attempted);
-                            return nextIndex;
-                        }
-                    }
-
-                    if (equivalentIndex >= destinationIndex)
-                    {
-                        if (!attempted.Contains(Direction.LEFT))
-                        {
-                            direction = Direction.LEFT;
-                            nextIndex = GetNextIndex(attempted);
-                            return nextIndex;
-                        }
-                    }
-
-                    if (!attempted.Contains(Direction.UP))
-                    {
-                        direction = Direction.UP;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-                }
-            }
-            else if (currentRow < destinationRow)
-            {
-                if (!attempted.Contains(Direction.UP) && lastDirection != Direction.UP)
-                {
-                    direction = Direction.UP;
-                    nextIndex = GetNextIndex(attempted);
-                    return nextIndex;
-                }
-                else
-                {
-                    if (equivalentIndex <= destinationIndex)
-                    {
-                        if (!attempted.Contains(Direction.RIGHT))
-                        {
-                            direction = Direction.RIGHT;
-                            nextIndex = GetNextIndex(attempted);
-                            return nextIndex;
-                        }
-                    }
-
-                    if (equivalentIndex >= destinationIndex)
-                    {
-                        if (!attempted.Contains(Direction.LEFT))
-                        {
-                            direction = Direction.LEFT;
-                            nextIndex = GetNextIndex(attempted);
-                            return nextIndex;
-                        }
-                    }
-
-                    if (!attempted.Contains(Direction.DOWN))
-                    {
-                        direction = Direction.DOWN;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-                }
-            }
-            else
-            {
-                if (equivalentIndex < destinationIndex)
-                {
-                    if (!attempted.Contains(Direction.RIGHT))
-                    {
-                        direction = Direction.RIGHT;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-
-                    if (!attempted.Contains(Direction.UP))
-                    {
-                        direction = Direction.UP;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-
-                    if (!attempted.Contains(Direction.DOWN))
-                    {
-                        direction = Direction.DOWN;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-                }
-
-                if (equivalentIndex > destinationIndex)
-                {
-                    if (!attempted.Contains(Direction.LEFT))
-                    {
-                        direction = Direction.LEFT;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-
-                    if (!attempted.Contains(Direction.UP))
-                    {
-                        direction = Direction.UP;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-
-                    if (!attempted.Contains(Direction.DOWN))
-                    {
-                        direction = Direction.DOWN;
-                        nextIndex = GetNextIndex(attempted);
-                        return nextIndex;
-                    }
-                }
-
-            }
-
-            return nextIndex;
-        }
-    }
+ 
 
 
 
 
 
-    /*private bool IsDestinationReached()
-    {
-        if (currentRow == destinationRow && currentColumn == destinationColumn)
-        {
-            return true;
-        }
-
-        return false;
-    }*/
+    
 
     private bool IsTileWalkable(int index)
     {
         return gridGO.transform.GetChild(index).GetComponent<Tile>().IsWalkable();
-    }
-
-    private bool IsValidNextIndex(int index)
-    {
-        if (index < 0 || index >= rows * columns)
-            return false;
-
-        if (direction == Direction.LEFT || direction == Direction.RIGHT)
-        {
-            if (index / columns != currentIndex / columns)
-                return false;
-        }
-
-        return IsTileWalkable(index);
-    }
-
-    void SetWeightAndDirection(int index)
-    {
-        var column = index % columns + 1;
-        var columnsToRight = columns - column;
-        var columnsToLeft = column - 1;
-        var row = index / columns + 1;
-        var rowsToUp = rows - row;
-        var rowsToDown = row - 1;
-
-        var gridT = gridGO.transform;
-        
-        for(var i=0;i< columnsToRight;++i)
-        {
-            var currentWeight = gridT.GetChild(index + i).GetComponent<Tile>().Weight;
-            var nextIndex = index + i + 1;
-            gridT.GetChild(nextIndex).GetComponent<Tile>().SetWeight(currentWeight);
-
-            for(var k=0;k<rowsToUp;k++)
-            {
-                currentWeight = gridT.GetChild(nextIndex + k * columns).GetComponent<Tile>().Weight;
-                gridT.GetChild(nextIndex + (k + 1) * columns).GetComponent<Tile>().SetWeight(currentWeight);
-            }
-
-            for(var k=0;k<rowsToDown;k++)
-            {
-                currentWeight = gridT.GetChild(nextIndex - k * columns).GetComponent<Tile>().Weight;
-                gridT.GetChild(nextIndex - (k + 1) * columns).GetComponent<Tile>().SetWeight(currentWeight);
-            }
-        }
-        
-        for (var i=0;i<columnsToLeft;++i)
-        {
-            var currentWeight = gridT.GetChild(index - i).GetComponent<Tile>().Weight;
-            var nextIndex = index - i - 1;
-            gridT.GetChild(nextIndex).GetComponent<Tile>().SetWeight(currentWeight);
-
-            for (var k = 0; k < rowsToUp; k++)
-            {
-                currentWeight = gridT.GetChild(nextIndex + k * columns).GetComponent<Tile>().Weight;
-                gridT.GetChild(nextIndex + (k + 1) * columns).GetComponent<Tile>().SetWeight(currentWeight);
-            }
-
-            for (var k = 0; k < rowsToDown; k++)
-            {
-                currentWeight = gridT.GetChild(nextIndex - k * columns).GetComponent<Tile>().Weight;
-                gridT.GetChild(nextIndex - (k + 1) * columns).GetComponent<Tile>().SetWeight(currentWeight);
-            }
-        }
-
-        for (var i = 0; i < rowsToUp; ++i)
-        {
-            var currentWeight = gridT.GetChild(index + i*columns).GetComponent<Tile>().Weight;
-            gridT.GetChild(index + (i + 1)*columns).GetComponent<Tile>().SetWeight(currentWeight);
-        }
-
-        for (var i = 0; i < rowsToDown; ++i)
-        {
-            var currentWeight = gridT.GetChild(index - i * columns).GetComponent<Tile>().Weight;
-            gridT.GetChild(index - (i + 1) * columns).GetComponent<Tile>().SetWeight(currentWeight);
-        }
-
-        /*if(IsValidLeft(index))
-        {
-            //gridT.GetChild(index - 1).GetComponent<Tile>().SetWeight(currentWeight);
-            //SetWeightAndDirection(index - 1);
-        }
-
-        if (IsValidUp(index))
-        {
-            //gridT.GetChild(index + columns).GetComponent<Tile>().SetWeight(currentWeight);
-            //SetWeightAndDirection(index + columns);
-        }
-
-        if(IsValidDown(index))
-        {
-            //gridT.GetChild(index - columns).GetComponent<Tile>().SetWeight(currentWeight);
-            //SetWeightAndDirection(index - columns);
-        }*/
-
     }
 
     private bool IsValidRight(int index)
